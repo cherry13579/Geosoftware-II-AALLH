e38a7ae0e4d2c7a3a614351c60d0b9865036c5f5
Ani <a_grau05@wwu.de>
2019-01-28 23:39:48 +0100

    API finished and testdata in the database

--- pycsw/pycsw/core/similaritycalculation.py
+++ pycsw/pycsw/core/similaritycalculation.py
@@ -2,9 +2,9 @@ import logging
 import os
 import sqlite3
 from math import floor 
-from pycsw.similaritycalculation import timeSimilarity as ts
-from pycsw.similaritycalculation import spatialSimilarity as sps
-from pycsw.similaritycalculation import generalSimilarity as gs
+from pycsw.core import timeSimilarity as ts
+from pycsw.core import spatialSimilarity as sps
+from pycsw.core import generalSimilarity as gs
 
 LOGGER = logging.getLogger(__name__)
 
