9ef576735b7d0f18221d381177b88d8b1bda23f2
Henry Fock <h_fock01@wwu.de>
2019-01-29 13:21:09 +0100

    kommentare und loggs in similaritycalculation

--- pycsw/similaritycalculation/timeSimilarity.py
+++ pycsw/similaritycalculation/timeSimilarity.py
@@ -8,31 +8,44 @@ LOGGER = logging.getLogger(__name__)
 #LOGGER.warning()
 
 def timeLength(timeA, timeB):
+    """calculates how similar two timelines are in length (len(timeA)/len(timeB) where timeA >= timeB)
+    :param timeA [start, end] list with start and end date
+    :param timeB [start, end] list with start and end date
+    :returns: value [0,1] of how much the two lengths are similar
+    """
     startA = DateTime(timeA[0])
     endA = DateTime(timeA[1])
 
     startB = DateTime(timeB[0])
     endB = DateTime(timeB[1])
 
+    # check if values are invalid or if an end date is before its start date
     if startA > endA or startB > endB:
         raise AttributeError("start value is higher than end value")
 
+    # get timelength of timeA and timeB (calculation is done automaticly by DateTime)
     lengthA = endA - startA
     lengthB = endB - startB
 
+    # prevent division by zero if both times have length 0
     if lengthA == lengthB:
         return 1
+    # calculate similarity
     else:
         if lengthA > lengthB:
             lengthPercentage = lengthB/lengthA
-            lengthPercentage = floor(lengthPercentage*100)/100
             return lengthPercentage
         else:
             lengthPercentage = lengthA/lengthB
-            lengthPercentage = floor(lengthPercentage*100)/100
             return lengthPercentage
 
 def timeOverlap(timeA, timeB):
+    """calculates how much timeA and timeB overlap eachother
+    :param timeA [start, end] list with start and end date
+    :param timeB [start, end] list with start and end date
+    :returns: percentage of overlap
+    """
+    # calculate the overlapping time
     if DateTime(timeA[0]) == DateTime(timeB[0]) and DateTime(timeA[1]) == DateTime(timeB[1]):
         # timeA and timeB are equal
         return 1
@@ -51,7 +64,8 @@ def timeOverlap(timeA, timeB):
             return 0
         else: # overlap
             overlap = DateTime(timeB[1]) - DateTime(timeA[0])
-        
+    
+    # calculate timelength of timeA and timeB (calculation done by DateTime)
     timeLengthA = DateTime(timeA[1]) - DateTime(timeA[0])
     timeLengthB = DateTime(timeB[1]) - DateTime(timeB[0])
 
@@ -61,29 +75,23 @@ def timeOverlap(timeA, timeB):
     if timeLengthB == 0:
         timeLengthB = 0.00001
 
+    # calculate percentage of overlap (overlap/longer time sequence)
     if timeLengthA > timeLengthB:
         overlapPercentage = overlap/timeLengthA
-        overlapPercentage = floor(overlapPercentage*100)/100
         return overlapPercentage
     else:
         overlapPercentage = overlap/timeLengthB
-        overlapPercentage = floor(overlapPercentage*100)/100
         return overlapPercentage
 
-def similarInterval(timeA, timeB):
-    if timeA[2] == timeB[2]:
-        return 1
-    else:
-        if timeA[2] > timeB[2]:
-            intervalPercentage = timeB[2]/timeA[2]
-            intervalPercentage = floor(intervalPercentage*100)/100
-            return intervalPercentage
-        else:
-            intervalPercentage = timeA[2]/timeB[2]
-            intervalPercentage = floor(intervalPercentage*100)/100
-            return intervalPercentage
-
-# print(timeOverlap(['1935/01/01 00:00:00 GMT+0', '2014/01/01 00:00:00 GMT+0', 365.253164556962], ['2013/01/01 00:00:00 GMT+0', '2018/03/28 12:43:14.034000 GMT+0', 0]))
-
-# print(DateTime('1935/01/01 00:00:10 GMT+0') - DateTime('1935/01/01 00:00:00 GMT+0'))
-print(timeLength(['1956/01/01 00:00:00 GMT+0', '2088/01/01 00:00:00 GMT+0', 730.5], ['1957/01/01 00:00:00 GMT+0', '2088/01/01 00:00:00 GMT+0', 123]))
+# def similarInterval(timeA, timeB):
+#     if timeA[2] == timeB[2]:
+#         return 1
+#     else:
+#         if timeA[2] > timeB[2]:
+#             intervalPercentage = timeB[2]/timeA[2]
+#             intervalPercentage = floor(intervalPercentage*100)/100
+#             return intervalPercentage
+#         else:
+#             intervalPercentage = timeA[2]/timeB[2]
+#             intervalPercentage = floor(intervalPercentage*100)/100
+#             return intervalPercentage
